7b2735fc3bb32a9df74b41ce88de2544f1d96c67
IngDeiver <deiver.guerra.2017@upb.edu.co>
2021-12-13 12:34:27 -0500

    Proyecto de grado publicado y cv mejorado

--- src/components/sobremi.js
+++ src/components/sobremi.js
@@ -1,39 +1,51 @@
 import React from "react"
-import SEO from './seo'
-import Avatar from './avatar'
-import Btn from '../styled/components/btn'
-import Fade from 'react-reveal/Fade';
-import LogoAndTextContainer from './logoAndTextContainer'
-import TextContainer from '../styled/components/pContainer'
-import { graphql, useStaticQuery } from 'gatsby'
+import SEO from "./seo"
+import Avatar from "./avatar"
+import Btn from "../styled/components/btn"
+import Fade from "react-reveal/Fade"
+import LogoAndTextContainer from "./logoAndTextContainer"
+import TextContainer from "../styled/components/pContainer"
+import { graphql, useStaticQuery } from "gatsby"
 
 // Este es el componente donde hablo sobre mí
 export default () => {
-
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query MyQuery {
-        strapiSobremis(id: {eq: "Sobremis_6001e9fdd06b8f54e53d0be6"}) {
-          descripcion
-        }
+      strapiSobremis(id: { eq: "Sobremis_6001e9fdd06b8f54e53d0be6" }) {
+        descripcion
       }
-    `)
+    }
+  `)
 
-    return (
-        <>
-            <SEO title="Sobre mí" />
-            <Avatar />
-            <LogoAndTextContainer>
-                <TextContainer>
-                  <p style={{textAlign:"justify"}}>
-                    {data.strapiSobremis?.descripcion}
-                  </p>
-                </TextContainer>
-            </LogoAndTextContainer>
-            <Fade bottom>
-                <div className="d-flex justify-content-center mt-3">
-                    <Btn to="/proyectos">Conoce mis proyectos</Btn>
-                </div>
-            </Fade>
-        </>
-    )
+  return (
+    <>
+      <SEO title="Sobre mí" />
+      <Avatar />
+      <LogoAndTextContainer>
+        <TextContainer>
+          <p style={{ textAlign: "justify" }}>
+            {data.strapiSobremis?.descripcion}
+          </p>
+        </TextContainer>
+        <div className="row">
+        <div className="col">
+          <Fade bottom>
+            <div className="d-flex justify-content-center mt-3">
+              <Btn to="/proyectos">Conoce mis proyectos</Btn>
+            </div>
+          </Fade>
+        </div>
+        <div className="col">
+          <Fade bottom>
+            <div className="d-flex justify-content-center mt-3">
+              <Btn to="/proyectos/proyecto-de-grado-expocheck">
+                Conoce mi proyecto de grado
+              </Btn>
+            </div>
+          </Fade>
+        </div>
+      </div>
+      </LogoAndTextContainer>
+    </>
+  )
 }
